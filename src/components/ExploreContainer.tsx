aae731eccc12f06ea6785bdf5ef3b6b347ad95af
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-08 11:36:19 +1300

    Renames tabs throughout project.

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,11 +5,12 @@ interface ContainerProps {
   name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC<ContainerProps> = ({name}) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI
+        Components</a></p>
     </div>
   );
 };
--- src/pages/SettingsTab.tsx
+++ src/pages/SettingsTab.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './SettingsTab.css';
 
-const Tab3: React.FC = () => {
+const SettingsTab: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -17,10 +17,10 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name="Tab 3 page"/>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default SettingsTab;
